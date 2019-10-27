9aba3d769227779995ba6907f39417a009acd31d
Kutlu Sahin <kutlugsahin@gmail.com>
2019-10-27 19:17:27 +0100

    config

---
+++ Components/Card.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.scss';
+
+export const Card = () => {
+	return (
+		<div className={styles.card}> 
+			Card
+		</div>
+	)
+}
--- next.config.js
+++ next.config.js
@@ -1,21 +1,40 @@
-const withSass = require('@zeit/next-sass');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = withSass({
-    cssModules: true,
-    cssLoaderOptions: {
-        camelCase: true,
-        namedExport: true,
-        
-    },
-    webpack(config, options) {
-        if (!options.isServer) {
-            for (let entry of options.defaultLoaders.sass) {
-                if (entry.loader === 'css-loader') {
-                    entry.loader = 'typings-for-css-modules-loader';
-                    break;
-                }
-            }
-        }
+module.exports = {
+    webpack: (config, options) => {
+        config.plugins.push(new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // all options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+        }));
+
+        config.module.rules.push({
+            test: /\.s[ac]ss$/i,
+            use: [
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        // you can specify a publicPath here
+                        // by default it uses publicPath in webpackOptions.output
+                        // publicPath: '../',
+                        hmr: process.env.NODE_ENV === 'development',
+                    },
+                },
+                {
+                    loader: 'typings-for-css-modules-loader?modules&namedExport&camelCase',
+                    options: {
+                        modules: true, // this option must be enabled
+                        camelCase: true,
+                        namedExport: true,
+                        localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                    }
+                },
+                // Compiles Sass to CSS
+                'sass-loader',
+            ],
+        })
         return config;
-    },
-});
+    }
+};
